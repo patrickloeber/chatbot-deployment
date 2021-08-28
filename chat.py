d0eaf22f14199acbfb542fadfe9768897bf989d9
Patrick Loeber (Python Engineer) <pythonengineerman@gmail.com>
2021-08-28 15:28:39 +0200

    modify chat.py

--- chat.py
+++ chat.py
@@ -47,4 +47,15 @@ def get_response(msg):
     
     return "I do not understand..."
 
-    
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
