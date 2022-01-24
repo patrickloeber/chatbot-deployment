494da035fa3587797167aac3d041f09c2e38fcad
Daryl-Loo <48171744+Daryl-Loo@users.noreply.github.com>
2022-01-25 09:48:08 +1100

    vmware
    added vmware intents

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Daryl\Downloads\chatbot-deployment\chatbot-deployment\venv
+set VIRTUAL_ENV=C:\Users\Daryl\Documents\GitHub\chatbot-deployment\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
