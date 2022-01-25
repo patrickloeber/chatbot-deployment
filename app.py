060fcc3b34f8f5fac5c9e331f1b6fba8eaa502a6
Daryl-Loo <48171744+Daryl-Loo@users.noreply.github.com>
2022-01-25 11:08:21 +1100

    Changes from Nicholas (limit request)
    Changes from Nicholas.
    
    Resolved the Limit no. of requests per IP address to 2 - 3 per second using flask_limiter

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 # from flask_cors import CORS 
 from chat import get_response
-# from flask_limiter import Limiter
-# from flask_limiter.util import get_remote_address
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 # CORS(app)
+limiter = Limiter(app, key_func = get_remote_address)
 
-# Comment the next three lines if you are using CORS(app) for standalone frontend and uncomment all other commented lines
+# Comment out @app.get and index_get() if you are using CORS(app) for standalone frontend and uncomment all other commented lines
 @app.get("/")
+@limiter.limit("3/second; 200/minute; 1200/hour")
 def index_get():
     return render_template("base.html")
 
@@ -20,7 +22,6 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
-
 
 if __name__ == "__main__":
     app.run(debug = True)
